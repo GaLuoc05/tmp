9234130948ad8aecf2d1c3fa01e1bc9183263219
GaLuoc05 <167160538+GaLuoc05@users.noreply.github.com>
2024-12-06 19:18:44 +0700

    update Dialogues_py/To_Phu.py

---
+++ Dialogues_py/To_Phu.py
@@ -0,0 +1,7 @@
+~ Talk_2
+Phú ngồi trên giường, đưa đầu lên xuống, mồm lẩm bẩm gì đó.
+Tô Phú: Mày đi đâu về muộn vậy?
+- Bạn: Lại tập nhịp trống à?
+    Tô Phú: Đánh kiếm tao đã không lại thằng Kim thì chí ít cũng phải đánh trống hơn nó.
+    
+    
