77d71b652509c79b35f62883876da47e00706bcd
GSlay <mha220871@gmail.com>
2024-12-31 18:24:56 +0700

    update

--- Dialogues_py/Coi_Khung.gd
+++ Dialogues_py/Coi_Khung.gd
@@ -1,26 +1,25 @@
 ~ Gossip
-- Bạn: Nói về Y Lão.[if talker.has_met("Y_Lao")]
-	- Còi Khùng: "Lọm khọm, khụ khụ." Hắn cười toáng lên. Khom lưng lại bắt chước lão già. "Tao sẽ làm thuốc nếu mày làm trò."
+- Nói về Y Lão.[if talker.has_met("Y_Lao")]
+	{{talker.name[0]}}: "Lọm khọm, khụ khụ." Hắn cười toáng lên. Khom lưng lại bắt chước lão già. "Tao sẽ làm thuốc nếu mày làm trò."
 ~ Talk_1
-if talker.att["perception"]["sight"] >= 2:
-	Thằng Còi nằm giữa đường như cây sậy cản bất cứ ai muốn qua. Hắn cứ ngồi đó từ sáng đến tối.
-	if talker.att["perception"]["hearing"] >= 2:
-		Còi Khùng: "Nhóc không nên đi lên đó đâu! Trên đó đỏ lắm!"
-		- Bạn: "Tại sao?" [if 9 > talker.att["iq"]["sum"] and talker.att["iq"]["sum"] > 3]
-		Còi Khùng: Hắn khua tay - "Biết vậy là được!" => Talk_1
-		- Bạn: "Màu đỏ?" [if talker.att["iq"]["sum"] > 6]
-		Còi Khùng: Gật đầu. - "ĐỎ thẫm! Nhìn sợ bỏ xừ..." => Talk_1
-		- Bạn: "Đỏ! Máu? Đỏ?" [if talker.att["iq"]["sum"] < 3]
-		Còi Khùng nhìn bạn chăm chú, như nhìn một hình bóng xưa. 
-		Còi Khùng: Cười toáng lên. - "Nhóc cũng thấy à? Đừng dại mà lên đó!"
-		- Bạn: "Đỏ kệ nó! Tránh ra mau." [if talker.att["iq"]["sum"] > 3]
-		Còi Khùng: Cười một tràng điên dại. - "Qua được thì cứ tự nhiên."
-		Nói xong hắn bước ra đứng chắn giữa đường. => END
-		- Rời đi. => END
+Một tên còi cọc nằm giữa đường như cây sậy cản bất cứ ai muốn qua. Hắn cứ ngồi đó không chịu di chuyển tí nào.
+if talker.att["perception"]["hearing"] >= 2:
+	{{talker.name[0]}}: "Nhóc không nên đi lên đó đâu! Trên đó đỏ lắm!"
+	- {{player.name}}: "Tại sao?" [if 9 > talker.att["iq"]["sum"] and talker.att["iq"]["sum"] > 3]
+	{{talker.name[0]}}: Hắn khua tay - "Biết vậy là được!" => Talk_1
+	- {{player.name}}: "Màu đỏ?" [if talker.att["iq"]["sum"] > 6]
+	{{talker.name[0]}}: Gật đầu. - "ĐỎ thẫm! Nhìn sợ bỏ xừ..." => Talk_1
+	- {{player.name}}: "Đỏ! Máu? Đỏ?" [if talker.att["iq"]["sum"] < 3]
+	{{talker.name[0]}} nhìn {{player.name}} chăm chú, như nhìn một hình bóng xưa. 
+	{{talker.name[0]}}: Cười toáng lên. - "Nhóc cũng thấy à? Đừng dại mà lên đó!"
+	- {{player.name}}: "Đỏ kệ nó! Tránh ra mau." [if talker.att["iq"]["sum"] > 3]
+	{{talker.name[0]}}: Cười một tràng điên dại. - "Qua được thì cứ tự nhiên."
+	Nói xong hắn bước ra đứng chắn giữa đường. => END
+	- Rời đi. => END
 		
 ~ Talk_2
-Còi Khùng vẫn nằm giữa đường, đúng cái vị trí mà hôm qua hắn nằm, không chệch một li.
-Còi Khùng: "Kiên trì không lại tao đâu nhóc."
-- Bạn: "Tránh ra mau!"
-	Còi Khùng thở dài rồi đứng dậy. => END
+{{talker.name[0]}} vẫn nằm giữa đường, đúng cái vị trí mà hôm qua hắn nằm, không chệch một li.
+{{talker.name[0]}}: "Kiên trì không lại tao đâu nhóc."
+- {{player.name}}: "Tránh ra mau!"
+	{{talker.name[0]}} thở dài rồi đứng dậy. => END
 		
--- Dialogues_py/Y_Lao.gd
+++ Dialogues_py/Y_Lao.gd
@@ -1,11 +1,8 @@
 ~ Talk_1
-if player.att["perception"]["sight"] >= 2:
-	{{talker.name[0]}}: Lão già đang bận bịu trong góc phòng.
-	if player.att["perception"]["sight"] > 4:
-		set player.appearance[0] = true
-		{{talker.name[0]}}: Ông nom gầy gò nhưng từng hành động vẫn toát lên sức khỏe. 
-if player.att["perception"]["sight"] == 1:
-	{{talker.name[0]}}: Bạn thấy mập mờ một thứ gì đó đang chuyển động.
+{{talker.name[0]}}: Lão già đang bận bịu trong góc phòng.
+if player.att["perception"]["sight"] > 4:
+	set player.appearance[0] = true
+	{{talker.name[0]}}: Ông nom gầy gò nhưng từng hành động vẫn toát lên sức khỏe. 
 {{talker.name[0]}}: ...
 if player.att["perception"]["sight"] > 2:
 	Ông lão đang nấu một món[if player.att["iq"]["science"] > 2] canh thịt với rau, bổ thân[else] canh gì đó[/if].
@@ -17,12 +14,4 @@ if player.["perception"]["smell"] > 2:
 	if player.att_max == "wpw": Bạn thèm! Bạn muốn ăn đống thức ăn đó, ăn tới hết thì thôi.
 	if player.att_max == "iq": Dù đói hay không, bạn sẽ cảm thấy tốt hơn nhiều với đống đồ ăn đó trong miệng.
 	if player.att_max == "bmi": Phần ăn đủ sức cho bạn ăn tới nổ cái bụng!
-if player.att["perception"]["hearing"] == 1:
-	{{talker.name[0]}}: A a a à?
-	- Không làm gì cả.
-		if player.att["iq"]["language"] < 1: Bạn muốn nói gì đó. Ra hiệu gì đó. Nhưng bạn không biết phải làm gì, nên bạn cứ đứng đờ ra đó, như một pho tượng lâu ngày bị rêu bám lấy.
-	if player.att["iq"]["language"] > 1:
-		- Ra hiệu rằng bạn không nghe được.
-			Bạn đưa tay lên tai rồi lắc đầu. Ông lão hiểu ngay tức khắc, ổng đưa cái muỗng đập đập vào[if player.att["perception"]["sight"] > 2] thành của nồi canh[else] viền kim loại gì đó[/if].
-else:
-	{{talker.name[0]}}:[if player.att["perception"]["hearing"] > 2] Giọng lão khàn mà ồm, chậm rãi.[/if] 
+{{talker.name[0]}}:[if player.att["perception"]["hearing"] > 2] Giọng lão khàn mà ồm, chậm rãi.[/if] "Ngươi từ đâu tới?"
