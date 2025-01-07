dad8b3735df7606885c73067a44c2137ac632d6e
GSlay <galuoc05@gmail.com>
2025-01-07 23:02:22 +0700

    updates

---
+++ game_gd/Scene.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+class_name Scene
+
+"""
+Node gốc của 'world game' mỗi khi game chạy (bên cạnh UI).
+"""
+
+# VAR
+var player: Player
+var other_players: Array[OtherPlayer] # Các người chơi (coop) khác, nếu họ xuất hiện trong game.
+var items: Array[Item]
+var static_objects: Array[StaticObject]
+# Vars static xuất hiện khi vào game
+# var static_grounds: Array[StaticBody2D]
